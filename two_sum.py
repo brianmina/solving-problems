66be46e6e9d9f29f2213779c7cde65e3b60f3342
brianmina17 <brian.mina17@gmail.com>
2024-03-14 10:58:37 -0400

    problem number 4

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+
+
+def twosum(nums: list[int], target: int) -> list[int]:
+    """return the indices of the two numbers such that they up to the target."""
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == total:
+                return [i, j]
+
+
+input_list = [8, 2, 7, 8]
+total = 9
+
+d = twosum(input_list, total)
+print(d)
